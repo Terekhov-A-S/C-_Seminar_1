6537f197c15107aef66569aa184912cbaf190d2a
KoT <Kotto@student.su>
2022-08-11 21:39:08 +0400

    Added ignore

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear ();
+Console.WriteLine ("Искусственный интеллект Core 2.0 приветствует Вас! ");
+Console.Write ("Пожалуйста, введите первое число для сравнения ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+Console.Write ("Замечательно! Теперь введите второе число для сравнения ");
+int b = Convert.ToInt32 (Console.ReadLine ());
+Console.Write ("Превосходно! Введите и третье число ");
+int c = Convert.ToInt32 (Console.ReadLine ());
+int max = a;
+if (b >= a) {max = b;}
+else if (c >= a) {max = c;}
+Console.Write ($"Максимальным из введенных Вами чисел является {max}. Всего доброго! Буду рад пообщаться снова ");
